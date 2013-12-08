9e826818c054e62adbc08dc718ca859bd2649c4e
= <=>
2013-12-08 14:42:03 +0800

    add a printf after pull, when conflict

--- main.c
+++ main.c
@@ -7,5 +7,6 @@ void main()
   printf("readme changed in remote repo, and changed main.c to commit 
            test if ok to push\n");
   printf("changg main.c in remote repo, test if changed in main.c in local repo\n");
+  i will add a printf here!!!
   return;
 }
