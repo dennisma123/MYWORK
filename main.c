993b702205a4765cca5440beecd08182552e195f
= <=>
2013-12-08 14:17:12 +0800

    changed readme in remote repo, but not pull, and chang main.c test

--- main.c
+++ main.c
@@ -4,5 +4,7 @@ void main()
   int value;
   scanf("value is %d", &value);
   printf("changed in worksapce, and staged but not commit\n");
+  printf("readme changed in remote repo, and changed main.c to commit 
+           test if ok to push\n");
   return;
 }
