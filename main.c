d4c2a11514c6848b75830d2690bb09078cbd3aed
dennisma123 <dennisma.china@gmail.com>
2013-12-08 14:21:36 +0800

    changed both in remote repo and local repo

--- main.c
+++ main.c
@@ -6,5 +6,6 @@ void main()
   printf("changed in worksapce, and staged but not commit\n");
   printf("readme changed in remote repo, and changed main.c to commit 
            test if ok to push\n");
+  printf("changg main.c in remote repo, test if changed in main.c in local repo\n");
   return;
 }
